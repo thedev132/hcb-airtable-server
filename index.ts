b43de88ed3f433198237c661037ae452ce12b383
Mohamad Mortada <mmortada685@gmail.com>
2025-03-17 21:33:33 -0700

    small changes

--- index.ts
+++ index.ts
@@ -128,50 +128,64 @@ async function testRefresh() {
 }
 
 async function testGrant() {
+    console.log('Running this task every 5 minutes:', new Date().toLocaleTimeString());
     const projects = await prisma.project.findMany();
+    const activeProjects = projects.filter(project => project.active);
+    activeProjects.forEach(async function(project) {
+          const user = await prisma.user.findFirst({
+              where: {
+                  id: project.ownerId
+              },
+          });
+          if (user?.airtable_pat) {
+              let base = new Airtable({apiKey: user.airtable_pat}).base(project.airtable_base_id);
+              base(project.airtable_table).select({
+                  view: project.airtable_view
+              }).eachPage(async function page(records, fetchNextPage) {
+                  records.forEach(async function(record) {
+                      const isApproved = record.get(project.airtable_approval_id);
+                      const isGrantSent = record.get(project.airtable_grant_id);
+                      if ((isApproved || isApproved == "Approved") && !isGrantSent) {
+                          console.log('Sending grant to ' + record.get('First Name'));
+                          const email = record.get('Email');
+                          const response = await fetch(`${authBaseUrl}/organizations/${project.organization}/card_grants`, {
+                              method: "POST",
+                              headers: {
+                                  Authorization: `Bearer ${user.access_token}`,
+                                  "Content-Type": "application/json",
+                              },
+                              body: JSON.stringify({
+                                  email: email,
+                                  amount_cents: project.grantAmount,
+                                  merchant_lock: "",
+                                  category_lock: "",
+                                  keyword_lock: ""
+                              }),
+                          });
+                          let data = await response.json();
+                          record.patchUpdate({
+                              [project.airtable_grant_id]: true
+                          });
+                          prisma.automation.create({
+                              data: {
+                                  projectId: project.id,
+                                  recieverEmail: email ? email.toString() : '',
+                                  recieverName: record.get('First Name') + ' ' + record.get('Last Name'),
+                                  recieverId: record.id,
+                                  name: `Grant to ${record.get('First Name') + ' ' + record.get('Last Name')}`,
+                                  status: response.ok ? 'Success' : 'Failed',
+                              }
+                          })
   
-    for (const project of projects) {
-      const user = await prisma.user.findFirst({
-        where: { id: project.ownerId },
-      });
   
-      if (user?.airtable_pat) {
-        let base = new Airtable({ apiKey: user.airtable_pat }).base(project.airtable_base_id);
-  
-        await new Promise((resolve, reject) => {
-          base(project.airtable_table)
-            .select({ view: project.airtable_view })
-            .eachPage(
-              async function page(records, fetchNextPage) {
-                for (const record of records) {
-                  const isApproved = record.get(project.airtable_approval_id);
-                  const isGrantSent = record.get(project.airtable_grant_id);
-  
-                  if ((isApproved || isApproved == "Approved") && !isGrantSent) {
-                    console.log("Sending grant to " + record.get("First Name"));
-                    const email = record.get("Email");
-                    const response = { ok: true };
-  
-                    record.patchUpdate({
-                      [project.airtable_grant_id]: true,
-                    });
-  
-                    await prisma.automation.create({
-                      data: {
-                        projectId: project.id,
-                        recieverEmail: email ? email.toString() : "",
-                        recieverName: record.get("First Name") + " " + record.get("Last Name"),
-                        recieverId: record.id,
-                        name: `Grant to ${record.get("First Name") + " " + record.get("Last Name")}`,
-                        status: response.ok ? "Success" : "Failed",
-                      },
-                    });
-                  }
-                }
-                fetchNextPage();
-              });
-        });
-      }
-    }
+                      }
+                  });
+                  fetchNextPage();
+              }, function done(err) {
+                  if (err) { console.error(err); return; }
+          }
+          )};
+    });
   }  
 
+testGrant();
